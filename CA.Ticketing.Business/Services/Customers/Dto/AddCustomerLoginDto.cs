125220305c36dca1ba1c51da52316db460cc9f20
Tarik Heleg <tarik.heleg1@gmail.com>
2023-07-25 11:34:08 +0200

    Added redirectUrl to AddLogin request
    Added redirectUrl to AddLogin request

---
+++ CA.Ticketing.Business/Services/Customers/Dto/AddCustomerLoginDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA.Ticketing.Business.Services.Customers.Dto
+{
+    public class AddCustomerLoginDto
+    {
+        public int customerContactId { get; set; }
+
+        public string redirectUrl { get; set; }
+    }
+}
