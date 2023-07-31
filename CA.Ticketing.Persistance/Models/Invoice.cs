e0f994ed563d2a4b3bebabff6c378c4433179ae7
tarikReadyDev <76901344+tarikheleg@users.noreply.github.com>
2023-07-31 11:21:25 +0200

    Invoices entity
    Invoices entity

---
+++ CA.Ticketing.Persistance/Models/Invoice.cs
@@ -0,0 +1,25 @@
+﻿using CA.Ticketing.Common.Constants;
+using CA.Ticketing.Persistance.Models.Abstracts;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA.Ticketing.Persistance.Models
+{
+    [Table(TableNames.Invoices)]
+    public class Invoice : IdentityModel<int>
+    {
+        public string InvoiceIdentifier { get; set; }
+
+        public DateTime InvoiceDate { get; set; }
+
+        public DateTime DueDate { get; set; }
+
+        public bool Paid { get; set; }
+
+        public virtual ICollection<FieldTicket> Tickets { get; set; } = new List<FieldTicket>();
+    }
+}
