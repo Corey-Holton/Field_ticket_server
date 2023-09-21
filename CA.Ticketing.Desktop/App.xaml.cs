3761c2fbdd46f85c18008fb70e99910fd3581f83
Nedim Sahovic <nedim.sahovic@readydev.tech>
2023-09-21 18:53:12 +0200

    Installation updates

--- CA.Ticketing.Desktop/App.xaml.cs
+++ CA.Ticketing.Desktop/App.xaml.cs
@@ -13,5 +13,15 @@ namespace CA.Ticketing.Desktop
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            this.DispatcherUnhandledException += App_DispatcherUnhandledException;
+        }
+
+        private void App_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show(e.Exception.Message);
+        }
     }
 }
